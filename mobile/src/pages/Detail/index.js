aea3f251341f39b37a40aa77dc874ddd475f0a1c
Raymw1 <rayan.rmw@gmail.com>
2022-04-12 22:37:25 -0300

    Mobile - page styles done

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,10 +1,47 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+import logoImg from "../../assets/logo.png";
+import styles from "./styles";
 
 export default function Detail() {
+  const navigation = useNavigation();
+
+  function navigateBack() {
+    navigation.goBack();
+  }
+
   return (
-    <View>
-      <Text>Detail</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <TouchableOpacity onPress={navigateBack}>
+          <Feather name="arrow-left" size={28} color="#e82041" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+        <Text style={styles.incidentValue}>APAD</Text>
+        <Text style={styles.incidentProperty}>INCIDENT:</Text>
+        <Text style={styles.incidentValue}>Atropelada dog</Text>
+        <Text style={styles.incidentProperty}>VALUE:</Text>
+        <Text style={styles.incidentValue}>R$ 120,00</Text>
+      </View>
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Save the day!</Text>
+        <Text style={styles.heroTitle}>Be the hero of this incident!</Text>
+        <Text style={styles.heroDescription}>Get in touch:</Text>
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}>WhatsApp</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}>E-mail</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,10 +1,53 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
+
+import logoImg from "../../assets/logo.png";
+import styles from "./styles";
 
 export default function Incidents() {
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate("Detail");
+  }
+
   return (
-    <View>
-      <Text>Incidents</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total of <Text style={styles.headerTextBold}>0 incidents</Text>.
+        </Text>
+      </View>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text style={styles.description}>
+        Choose on of the incidents below and save the day!
+      </Text>
+      <FlatList
+        data={[1, 2, 3, 4, 5]}
+        style={styles.incidentList}
+        keyExtractor={(item) => String(item)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>APAD</Text>
+            <Text style={styles.incidentProperty}>INCIDENT:</Text>
+            <Text style={styles.incidentValue}>Atropelada dog</Text>
+            <Text style={styles.incidentProperty}>VALUE:</Text>
+            <Text style={styles.incidentValue}>R$ 120,00</Text>
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={navigateToDetail}
+            >
+              <Text style={styles.detailsButtonText}>More details</Text>
+              <Feather name="arrow-right" size={16} color="#E02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
